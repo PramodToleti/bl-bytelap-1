d2c16125ae0f4ed2a233ea0022f05eaaf6325954
PramodToleti <pramodraina047@gmail.com>
2023-01-08 17:20:51 +0530

    Added Checkbox Dropdown in Step-1

--- src/register_myself/experience/Experience1/index.js
+++ src/register_myself/experience/Experience1/index.js
@@ -1,117 +1,218 @@
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
+import { Component } from "react"
+import { default as ReactSelect } from "react-select"
+import { components } from "react-select"
 
 import "./index.css"
 
-const Experience1 = (props) => {
-  const { updateStep2 } = props
-  const onClickSubmit = (e) => {
+const fulltime = [
+  { value: "Full time", label: "Full time" },
+  { value: "Part time", label: "Part Time" },
+  { value: "Both", label: "Both" },
+]
+
+const schedule = [
+  { value: "Office", label: "Office" },
+  { value: "Remote", label: "Remote" },
+  { value: "Flexible", label: "Flexible" },
+]
+
+const shift = [
+  { value: "Day shift", label: "Day shift" },
+  { value: "Night shift", label: "Morning shift" },
+  { value: "Rotational shift", label: "Rotational shift" },
+]
+const Option = (props) => {
+  return (
+    <div>
+      <components.Option {...props}>
+        <input
+          type="checkbox"
+          checked={props.isSelected}
+          onChange={() => null}
+        />{" "}
+        <label>{props.label}</label>
+      </components.Option>
+    </div>
+  )
+}
+
+class Experience1 extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      optionSelected: null,
+    }
+  }
+
+  onClickSubmit = (e) => {
     e.preventDefault()
   }
 
-  const onClickSave = () => {
+  onClickSave = () => {
+    const { updateStep2 } = this.props
     updateStep2()
   }
 
-  return (
-    <>
-      <p className="step-text">Step 1-3</p>
-      <Form className="register-form" onSubmit={onClickSubmit}>
-        <Form.Group className="mb-4 input-field">
-          <Form.Check
-            type="checkbox"
-            id="checkbox"
-            label="Are you actively looking for job"
-          />
-        </Form.Group>
-        <Form.Group className="mb-4 input-field">
-          <Form.Label htmlFor="TextInput">Job Title</Form.Label>
-          <Form.Control
-            id="TextInput"
-            placeholder="Softwave developer, Digital marketing"
-          />
-        </Form.Group>
-        <Form.Group className="mb-4 input-field">
-          <Form.Label htmlFor="disabledSelect">
-            Looking for full time or part time
-          </Form.Label>
-          <Form.Select id="Select">
-            <option>Full time</option>
-            <option>Part time</option>
-            <option>Both</option>
-          </Form.Select>
-        </Form.Group>
+  handleChange = (selected) => {
+    this.setState({
+      optionSelected: selected,
+    })
+  }
 
-        <Form.Group className="mb-4 input-field">
-          <Form.Label htmlFor="disabledTextInput">
-            What is the schedule of the internship
-          </Form.Label>
-          <Form.Select id="Select">
-            <option>Office</option>
-            <option>Remote</option>
-            <option>Flexible</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-4 input-field">
-          <Form.Label htmlFor="disabledSelect">
-            In which shift you are looking for job?
-          </Form.Label>
-          <Form.Select id="Select">
-            <option>Day shift</option>
-            <option>Night shift</option>
-            <option>Rotational shift</option>
-          </Form.Select>
-        </Form.Group>
-        <Form.Group className="mb-4 input-field">
-          <Form.Label htmlFor="disabledSelect">Experience</Form.Label>
-          <Form.Select id="Select">
-            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((each) => (
-              <option>{each}</option>
-            ))}
-          </Form.Select>
-        </Form.Group>
-        <div className="form-row">
-          <div className="form-group col-6">
-            <label htmlFor="salary-expectation">Salary expectation</label>
-            <Form.Select
-              className="form-control"
-              id="salary-expectation"
-              placeholder="Range type"
+  render() {
+    return (
+      <>
+        <p className="step-text">Step 1-3</p>
+        <Form className="register-form" onSubmit={this.onClickSubmit}>
+          <Form.Group className="mb-4 input-field">
+            <Form.Check
+              type="checkbox"
+              id="checkbox"
+              label="Are you actively looking for job"
+            />
+          </Form.Group>
+          <Form.Group className="mb-4 input-field">
+            <Form.Label htmlFor="TextInput">Job Title</Form.Label>
+            <Form.Control
+              id="TextInput"
+              placeholder="Softwave developer, Digital marketing"
+            />
+          </Form.Group>
+          <Form.Group className="mb-4 input-field">
+            <Form.Label htmlFor="disabledSelect">
+              Looking for full time or part time
+            </Form.Label>
+            <span
+              className="input-field"
+              data-toggle="popover"
+              data-trigger="focus"
+              data-content="Please selecet account(s)"
+              placeholder="Select an option"
             >
-              <option>Not Disclosed</option>
-              <option>Lac</option>
-              <option>Per Month</option>
-              <option>Fixed</option>
+              <ReactSelect
+                options={fulltime}
+                isMulti
+                closeMenuOnSelect={false}
+                hideSelectedOptions={false}
+                components={{
+                  Option,
+                }}
+                onChange={this.handleChange}
+                allowSelectAll={true}
+                value={this.state.optionSelected}
+                placeholder="Select an option"
+              />
+            </span>
+          </Form.Group>
+          <Form.Group className="mb-4 input-field">
+            <Form.Label htmlFor="disabledTextInput">
+              What is the schedule of the internship
+            </Form.Label>
+            <span
+              className="input-field"
+              data-toggle="popover"
+              data-trigger="focus"
+              data-content="Please selecet account(s)"
+              placeholder="Select an option"
+            >
+              <ReactSelect
+                options={schedule}
+                isMulti
+                closeMenuOnSelect={false}
+                hideSelectedOptions={false}
+                components={{
+                  Option,
+                }}
+                onChange={this.handleChange}
+                allowSelectAll={true}
+                value={this.state.optionSelected}
+                placeholder="Select an option"
+              />
+            </span>
+          </Form.Group>
+          <Form.Group className="mb-4 input-field">
+            <Form.Label htmlFor="disabledTextInput">
+              In which shift you are looking for job?
+            </Form.Label>
+            <span
+              className="input-field"
+              data-toggle="popover"
+              data-trigger="focus"
+              data-content="Please selecet account(s)"
+              placeholder="Select an option"
+            >
+              <ReactSelect
+                options={shift}
+                isMulti
+                closeMenuOnSelect={false}
+                hideSelectedOptions={false}
+                components={{
+                  Option,
+                }}
+                onChange={this.handleChange}
+                allowSelectAll={true}
+                value={this.state.optionSelected}
+                placeholder="Select an option"
+              />
+            </span>
+          </Form.Group>
+          <Form.Group className="mb-4 input-field">
+            <Form.Label htmlFor="disabledSelect">Experience</Form.Label>
+            <Form.Select id="Select">
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((each) => (
+                <option>{each}</option>
+              ))}
             </Form.Select>
+          </Form.Group>
+          <div className="form-row">
+            <div className="form-group col-6">
+              <label htmlFor="salary-expectation">Salary expectation</label>
+              <Form.Select
+                className="form-control"
+                id="salary-expectation"
+                placeholder="Range type"
+              >
+                <option>Not Disclosed</option>
+                <option>Lac</option>
+                <option>Per Month</option>
+                <option>Fixed</option>
+              </Form.Select>
+            </div>
+            <div className="form-group col-3">
+              <label htmlFor="min">Min</label>
+              <input type="text" className="form-control" id="min" />
+            </div>
+            <div className="form-group col-3">
+              <label htmlFor="max">Max</label>
+              <input type="text" className="form-control" id="max" />
+            </div>
           </div>
-          <div className="form-group col-3">
-            <label htmlFor="min">Min</label>
-            <input type="text" className="form-control" id="min" />
-          </div>
-          <div className="form-group col-3">
-            <label htmlFor="max">Max</label>
-            <input type="text" className="form-control" id="max" />
-          </div>
-        </div>
-        <Form.Group className="mb-4 input-field">
-          <Form.Label htmlFor="TextInput">
-            Which location do you prefer looking for internship ?
-          </Form.Label>
-          <Form.Control id="TextInput" />
-        </Form.Group>
-        <Form.Group className="mb-4 input-field">
-          <Form.Label htmlFor="TextInput">
-            Anymore location (optional)
-          </Form.Label>
-          <Form.Control id="TextInput" />
-        </Form.Group>
+          <Form.Group className="mb-4 input-field">
+            <Form.Label htmlFor="TextInput">
+              Which location do you prefer looking for internship ?
+            </Form.Label>
+            <Form.Control id="TextInput" />
+          </Form.Group>
+          <Form.Group className="mb-4 input-field">
+            <Form.Label htmlFor="TextInput">
+              Anymore location (optional)
+            </Form.Label>
+            <Form.Control id="TextInput" />
+          </Form.Group>
 
-        <Button type="submit" className="submit-button" onClick={onClickSave}>
-          Save & Next
-        </Button>
-      </Form>
-    </>
-  )
+          <Button
+            type="submit"
+            className="submit-button"
+            onClick={this.onClickSave}
+          >
+            Save & Next
+          </Button>
+        </Form>
+      </>
+    )
+  }
 }
 
 export default Experience1
