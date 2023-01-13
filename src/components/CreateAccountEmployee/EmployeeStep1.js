996f058e2a7c4dd7190b325d7937774fd2c008a9
Pramod <pramodraina047@gmail.com>
2023-01-13 11:05:53 +0530

    Added Create account components for employee

---
+++ src/components/CreateAccountEmployee/EmployeeStep1.js
@@ -0,0 +1,107 @@
+import FloatingLabel from "react-bootstrap/FloatingLabel"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+
+import React, { useState } from "react"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
+import { Link } from "react-router-dom"
+
+function EmployeeStep1() {
+  const [validated, setValidated] = useState(false)
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget
+    if (form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    setValidated(true)
+  }
+
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div className="col-lg-4 col-md-4 search-course-right   mb-4 mt-4 p-2 bg-light text-dark  border-secondary rounded container reveal  p-3 mb-5 bg-white rounded border border-secondary">
+        <Form
+          action=""
+          noValidate
+          validated={validated}
+          onSubmit={handleSubmit}
+        >
+          <Row className="mb-3">
+            <p className="text-end mt-1">Step 1-2</p>
+            <p className="text-center mt-3">Create Employer Account</p>
+            <Form.Group as={Col} md="12" controlId="validationCustom01">
+              <FloatingLabel controlId="floatingText" label="Company Name">
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Demo Solution inc"
+                  defaultValue=""
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your company name.
+                </Form.Control.Feedback>
+              </FloatingLabel>
+            </Form.Group>
+
+            <Form.Group
+              as={Col}
+              md="12"
+              className="mt-3"
+              controlId="validationCustom01"
+            >
+              <FloatingLabel controlId="floatingText" label="Official Email ID">
+                <Form.Control
+                  required
+                  type="email"
+                  placeholder="Info@company.com"
+                  defaultValue=""
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter official email ID.
+                </Form.Control.Feedback>
+              </FloatingLabel>
+            </Form.Group>
+          </Row>
+
+          <button className=" mt-1 text-center btn btn-outline-primary">
+            Get Code
+          </button>
+          <button className=" mt-1 mx-1 text-center btn btn-outline-secondary">
+            Enter Code
+          </button>
+          <p className="text-start mt-1">
+            {" "}
+            <Link to="" style={{ textDecoration: "none" }}>
+              Resend again{" "}
+            </Link>
+          </p>
+
+          <div className="d-grid gap-2 mt-3">
+            <Link to="/employee/create-account/step-2">
+              <Button
+                type="submit"
+                variant="outline-secondary"
+                size="lg"
+                style={{ width: "100%" }}
+              >
+                Verify & Next
+              </Button>
+            </Link>
+          </div>
+        </Form>
+      </div>
+    </div>
+  )
+}
+
+export default EmployeeStep1
---
+++ src/components/CreateAccountEmployee/EmployeeStep2.js
@@ -0,0 +1,185 @@
+import Stack from "react-bootstrap/Stack"
+import Button from "react-bootstrap/Button"
+import Form from "react-bootstrap/Form"
+
+import React, { useState } from "react"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
+import { Link } from "react-router-dom"
+
+function EmployeeStep2() {
+  const [validated, setValidated] = useState(false)
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget
+    if (form.checkValidity() === false) {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+
+    setValidated(true)
+  }
+
+  return (
+    <div className="col-lg-4 col-md-4 search-course-right   mb-4 mt-4 p-2 bg-light text-dark  border-secondary rounded container reveal  p-3 mb-5 bg-white rounded border border-secondary">
+      <p className="text-end">Step 2-2</p>
+      <p className="text-center">Company Info</p>
+
+      <Form action="" noValidate validated={validated} onSubmit={handleSubmit}>
+        <Row className="mb-3">
+          <Form.Group as={Col} md="12" controlId="validationCustom01">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="First name"
+              defaultValue=""
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter your first.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group
+            as={Col}
+            md="12"
+            className="mt-3"
+            controlId="validationCustom01"
+          >
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              placeholder="Last name"
+              defaultValue=""
+            />
+            <Form.Control.Feedback type="invalid">
+              Please enter you last name.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3 mt-2">
+            <Form.Label> You Role </Form.Label>
+            <Form.Select>
+              <option> Select an option </option>
+              <option> Owner </option>
+              <option>Human Resource</option>
+              <option>Junior Human Resource</option>
+              <option>Marketing Specialist</option>
+              <option>Junior Marketing Specialist</option>
+              <option>CEO</option>
+              <option>CTO</option>
+              <option>Other</option>
+            </Form.Select>
+          </Form.Group>
+
+          <Form.Group
+            as={Col}
+            md="12"
+            className="mt-3"
+            controlId="validationCustom01"
+          >
+            <Form.Label>Company website</Form.Label>
+            <Form.Control
+              required
+              type="link"
+              placeholder="Paste URL"
+              defaultValue=""
+            />
+            <Form.Control.Feedback type="invalid">
+              Enter your company website URL.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group
+            as={Col}
+            md="12"
+            className="mt-3"
+            controlId="validationCustom01"
+          >
+            <Stack direction="horizontal" gap={3}>
+              <input className="d-none" type="file" />
+              <button className="btn btn-outline-primary">Upload Logo</button>
+            </Stack>
+            <Form.Control.Feedback type="invalid">
+              Please upload company logo.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group
+            className="mb-3 mt-3"
+            controlId="exampleForm.ControlTextarea1"
+          >
+            <Form.Label>Company Address</Form.Label>
+            <Form.Control
+              required
+              text="type"
+              as="textarea"
+              rows={2}
+              placeholder=""
+            />
+          </Form.Group>
+
+          <Form.Group
+            as={Col}
+            md="12"
+            className="mt-2"
+            controlId="validationCustom01"
+          >
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              required
+              type="link"
+              placeholder="Create password"
+              defaultValue=""
+            />
+            <Form.Control.Feedback type="invalid">
+              Please create your password
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group
+            as={Col}
+            md="12"
+            className="mt-3"
+            controlId="validationCustom01"
+          >
+            <Form.Label>Confirm Password</Form.Label>
+            <Form.Control
+              required
+              type="link"
+              placeholder="confirm password"
+              defaultValue=""
+            />
+            <Form.Control.Feedback type="invalid">
+              Please confirm your password
+            </Form.Control.Feedback>
+          </Form.Group>
+        </Row>
+
+        <Form.Group className="mb-3 mt-4">
+          <Form.Check
+            required
+            label="Agree to terms and conditions"
+            feedback="You must agree before submitting."
+            feedbackType="invalid"
+          />
+        </Form.Group>
+        <div className="d-grid gap-2 mt-2">
+          <Link to="/">
+            <Button
+              type="submit"
+              variant="outline-secondary"
+              size="lg"
+              style={{ width: "100%" }}
+            >
+              Submit
+            </Button>
+          </Link>
+        </div>
+      </Form>
+    </div>
+  )
+}
+
+export default EmployeeStep2
