7c6cf1d2ec5c5cc68ad988d78822a498ccf72175
PramodToleti <pramodraina047@gmail.com>
2023-03-15 20:01:10 +0530

    Updated Media Footer

--- src/components/Employee/FooterLinks/Media/index.js
+++ src/components/Employee/FooterLinks/Media/index.js
@@ -1,5 +1,33 @@
+import "./index.css"
+
 const Media = () => {
-  return <h1>Media</h1>
+  return (
+    <div className="container">
+      <h3 className="mb-4">Recent</h3>
+      <div className="main-image-container mb-3">
+        <p>Image</p>
+      </div>
+      <p
+        align="end"
+        style={{ color: "blue", fontSize: "16px" }}
+        className="mb-5"
+      >
+        Read More...
+      </p>
+
+      <div className="media-sub-images-container mb-5">
+        <div className="media-sub-images">
+          <p>Image</p>
+        </div>
+        <div className="media-sub-images">
+          <p>Image</p>
+        </div>
+        <div className="media-sub-images">
+          <p>Image</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Media
