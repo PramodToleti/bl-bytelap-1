bfab7c37d0255a321a07b4b9a76d769f06569fbe
PramodToleti <pramodraina047@gmail.com>
2023-04-14 18:56:26 +0530

    Updated Locations View in Jobs

--- client/src/components/Employee/EmployeeUnregistered/index.js
+++ client/src/components/Employee/EmployeeUnregistered/index.js
@@ -1,8 +1,9 @@
-import { Link } from "react-router-dom"
-import { useState, useEffect, useRef } from "react"
+import { Link, useHistory } from "react-router-dom"
+import { useState } from "react"
 import { Button, Card } from "react-bootstrap"
 import Slider from "react-slick"
-
+import { Oval } from "react-loader-spinner"
+import { ToastContainer, toast } from "react-toastify"
 import Popup from "reactjs-popup"
 import FloatingLabel from "react-bootstrap/FloatingLabel"
 import Form from "react-bootstrap/Form"
@@ -10,12 +11,14 @@ import Stack from "react-bootstrap/Stack"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 
+import "./index.css"
 import "reactjs-popup/dist/index.css"
 import "react-toastify/dist/ReactToastify.css"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
-
+import "react-toastify/dist/ReactToastify.css"
 import "bootstrap/dist/css/bootstrap.min.css"
+
 import Theme from "../../../assets/Theme"
 import AboutUs from "../FooterLinks/AboutUs"
 import Blog from "../FooterLinks/Blog"
@@ -26,15 +29,22 @@ import TermsAndConditions from "../FooterLinks/TermsAndConditions"
 import Startup from "../FooterLinks/Startup"
 import Enterprise from "../FooterLinks/Enterprise"
 import ChooseFile from "../../../assets/ChooseFile"
-
-import "./index.css"
 import SkeletonContainer from "../../../assets/SkeletonContainer"
 import UnregisteredNavBar from "../UnregisteredNavBar"
 import EmployeeFooter from "../EmployeeFooter"
 
 function EmployeeUnregistered() {
   const [isNext, setIsNext] = useState(false)
-
+  const history = useHistory()
+  const [validated, setValidated] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+  const [otp, setOtp] = useState("")
+  const [timeStamp, setTimeStamp] = useState("")
+  const [companyDetails, setCompanyDetails] = useState({
+    companyName: "",
+    officialEmail: "",
+    otp: "",
+  })
   const [jobs, setJobs] = useState([
     "IT Engineering",
     "BPO & KPO",
@@ -43,16 +53,6 @@ function EmployeeUnregistered() {
     "Marketing",
     "Finance",
   ])
-
-  /* const jobs = [
-    "IT Engineering",
-    "BPO & KPO",
-    "Operations",
-    "Sales & BD",
-    "Marketing",
-    "Finance",
-  ] */
-
   const settings = {
     dots: false,
     arrows: true,
@@ -69,6 +69,165 @@ function EmployeeUnregistered() {
     setIsPostActive(true)
   }
 
+  const onSuccessOtp = (message, otp) => {
+    setIsLoading(false)
+    toast.success(message, {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      style: {
+        border: "2px solid #00ff00",
+        backgroundColor: "#fff",
+        marginTop: "30px",
+        margin: "20px",
+      },
+    })
+    setOtp(otp)
+    setTimeStamp(Date.now())
+  }
+
+  const onFailureOtp = (message) => {
+    setIsLoading(false)
+    toast.error(message, {
+      position: "top-center",
+      autoClose: 3000,
+      hideProgressBar: true,
+      style: {
+        border: "2px solid #ff0000",
+        backgroundColor: "#fff",
+        marginTop: "30px",
+        margin: "20px",
+      },
+    })
+  }
+
+  const onGetCode = async () => {
+    if (companyDetails.officialEmail === "") {
+      toast.error("Email can't be Empty", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        style: {
+          border: "2px solid #ff0000",
+          backgroundColor: "#fff",
+          marginTop: "30px",
+          margin: "20px",
+        },
+      })
+      return
+    }
+    if (timeStamp === "" || Date.now() - timeStamp > 300000) {
+      setIsLoading(true)
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: companyDetails.officialEmail,
+        }),
+      }
+
+      const response = await fetch(
+        "http://localhost:5000/otp-verification/send-otp",
+        options
+      )
+      const data = await response.json()
+      if (response.ok) {
+        onSuccessOtp(data.message, data.otp)
+      } else {
+        onFailureOtp(data.message)
+      }
+    } else {
+      toast.error("OTP already sent", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        style: {
+          border: "2px solid #ff0000",
+          backgroundColor: "#fff",
+          marginTop: "30px",
+          margin: "20px",
+        },
+      })
+    }
+  }
+
+  const isFormFilled = () => {
+    for (const key in companyDetails) {
+      if (companyDetails.hasOwnProperty(key) && !companyDetails[key]) {
+        return false
+      }
+    }
+    return true
+  }
+
+  const isFormValid = (form) => {
+    for (let i = 0; i < form.length; i++) {
+      const input = form[i]
+      if (!input.validity.valid) {
+        return false
+      }
+    }
+    return true
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setValidated(true)
+    if (isFormFilled() && isFormValid(event.target)) {
+      if (parseInt(otp) !== parseInt(companyDetails.otp)) {
+        toast.error("Invalid OTP", {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: true,
+          style: {
+            border: "2px solid #ff0000",
+            backgroundColor: "#fff",
+            marginTop: "30px",
+            margin: "20px",
+          },
+        })
+        return
+      }
+      if (Date.now() - timeStamp > 300000) {
+        toast.error("OTP Expired", {
+          position: "top-center",
+          autoClose: 3000,
+          hideProgressBar: true,
+          style: {
+            border: "2px solid #ff0000",
+            backgroundColor: "#fff",
+            marginTop: "30px",
+            margin: "20px",
+          },
+        })
+        return
+      }
+      toast.success("Verified Successfully", {
+        position: "top-center",
+        autoClose: 3000,
+        hideProgressBar: true,
+        style: {
+          border: "2px solid #00ff00",
+          backgroundColor: "#fff",
+          marginTop: "30px",
+          margin: "20px",
+        },
+      })
+      setTimeout(() => {
+        setIsNext(true)
+      }, 1000)
+    } else {
+      console.log("Fill the form")
+    }
+  }
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setCompanyDetails({ ...companyDetails, [name]: value })
+  }
+
   const renderSteps = () => {
     if (!isNext) {
       return (
@@ -76,10 +235,17 @@ function EmployeeUnregistered() {
           className="col-lg-4 col-md-4 search-course-right   mb-4 mt-4 p-2      border-secondary rounded container reveal  p-3 mb-5   rounded border border-secondary"
           style={{ width: "100%" }}
         >
-          <Form action="" noValidate>
+          <ToastContainer />
+          <Form
+            action=""
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+            onChange={handleInputChange}
+          >
             <Row className="mb-3">
               <p className="text-end mt-1">Step 1-2</p>
-              <p className="text-center mt-3">Create Employer Account</p>
+              <p className="text-center mt-3 mb-3">Create Employer Account</p>
               <Form.Group as={Col} md="12" controlId="validationCustom01">
                 <FloatingLabel controlId="floatingText" label="Company Name">
                   <Form.Control
@@ -87,6 +253,7 @@ function EmployeeUnregistered() {
                     type="text"
                     placeholder="Demo Solution inc"
                     defaultValue=""
+                    name="companyName"
                   />
                   <Form.Control.Feedback type="invalid">
                     Please enter your company name.
@@ -109,6 +276,7 @@ function EmployeeUnregistered() {
                     type="email"
                     placeholder="Info@company.com"
                     defaultValue=""
+                    name="officialEmail"
                   />
                   <Form.Control.Feedback type="invalid">
                     Please enter official email ID.
@@ -117,12 +285,65 @@ function EmployeeUnregistered() {
               </Form.Group>
             </Row>
 
-            <button className=" mt-1 text-center btn btn-outline-primary">
-              Get Code
-            </button>
-            <button className=" mt-1 mx-1 text-center btn btn-outline-secondary">
-              Enter Code
-            </button>
+            <div
+              style={{ display: "flex", gap: "10px", alignItems: "flex-start" }}
+            >
+              <div>
+                <button
+                  className=" mt-1 text-center btn btn-primary"
+                  type="button"
+                  onClick={onGetCode}
+                  style={{ display: "grid", placeItems: "center" }}
+                >
+                  {isLoading ? (
+                    <Oval
+                      height={20}
+                      width={20}
+                      color="#ffffff"
+                      wrapperStyle={{}}
+                      wrapperClass=""
+                      visible={true}
+                      ariaLabel="oval-loading"
+                      secondaryColor="#ffffff"
+                      strokeWidth={2}
+                      strokeWidthSecondary={2}
+                    />
+                  ) : (
+                    "Get Code"
+                  )}
+                </button>
+                {timeStamp && (
+                  <p
+                    style={{
+                      color: "#dc3545",
+                      fontSize: ".775rem",
+                      marginBottom: "0px",
+                      marginTop: "4px",
+                    }}
+                  >
+                    Expires in 5min
+                  </p>
+                )}
+              </div>
+
+              <Form.Group className="mt-1" controlId="otp">
+                <Form.Control
+                  required
+                  type="number"
+                  placeholder="Enter Code"
+                  name="otp"
+                  defaultValue=""
+                  style={{
+                    maxWidth: "120px",
+                    maxHeight: "40px",
+                    border: "1px solid grey",
+                  }}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter the Code
+                </Form.Control.Feedback>
+              </Form.Group>
+            </div>
             <p className="text-start mt-1">
               {" "}
               <Link to="" style={{ textDecoration: "none", color: "blue" }}>
@@ -136,7 +357,6 @@ function EmployeeUnregistered() {
                 variant="outline-secondary"
                 size="lg"
                 style={{ width: "100%" }}
-                onClick={() => setIsNext(true)}
               >
                 Verify & Next
               </Button>
@@ -150,176 +370,7 @@ function EmployeeUnregistered() {
       <div
         className="col-lg-4 col-md-4 search-course-right   mb-4 mt-4 p-2    border-secondary rounded container reveal  p-3 mb-5   rounded border border-secondary"
         style={{ width: "100%" }}
-      >
-        <p className="text-end">Step 2-2</p>
-        <p className="text-center">Company Info</p>
-
-        <Form action="" noValidate>
-          <Row className="mb-3">
-            <Form.Group as={Col} md="12" controlId="validationCustom01">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="First name"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter your first.
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Last name"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter you last name.
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group className="mb-3 mt-2">
-              <Form.Label> You Role </Form.Label>
-              <Form.Select>
-                <option> Select an option </option>
-                <option> Owner </option>
-                <option>Human Resource</option>
-                <option>Junior Human Resource</option>
-                <option>Marketing Specialist</option>
-                <option>Junior Marketing Specialist</option>
-                <option>CEO</option>
-                <option>CTO</option>
-                <option>Other</option>
-              </Form.Select>
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label>Company website</Form.Label>
-              <Form.Control
-                required
-                type="link"
-                placeholder="Paste URL"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Enter your company website URL.
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label className="mt-2">Upload Company Logo </Form.Label>
-              <Stack
-                direction="horizontal"
-                gap={3}
-                style={{
-                  marginTop: "7px",
-                  display: "flex",
-                  alignItems: "flex-start",
-                }}
-              >
-                <input className="d-none" type="file" />
-                <ChooseFile />
-              </Stack>
-              <Form.Control.Feedback type="invalid">
-                Please upload company logo.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <small className="text-start text-muted mt-3">
-              PDF,Doc,Docx, | Max:2MB
-            </small>
-
-            <Form.Group
-              className="mb-3 mt-3"
-              controlId="exampleForm.ControlTextarea1"
-            >
-              <Form.Label>Company Address</Form.Label>
-              <Form.Control
-                required
-                text="type"
-                as="textarea"
-                rows={2}
-                placeholder=""
-              />
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-2"
-              controlId="validationCustom01"
-            >
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                required
-                type="link"
-                placeholder="Create password"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please create your password
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control
-                required
-                type="link"
-                placeholder="confirm password"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please confirm your password
-              </Form.Control.Feedback>
-            </Form.Group>
-          </Row>
-
-          <Form.Group className="mb-3 mt-4">
-            <Form.Check
-              required
-              label="Agree to terms and conditions"
-              feedback="You must agree before submitting."
-              feedbackType="invalid"
-            />
-          </Form.Group>
-          <div className="d-grid gap-2 mt-2">
-            <Link to="/employee">
-              <Button
-                type="submit"
-                variant="outline-secondary"
-                size="lg"
-                style={{ width: "100%" }}
-              >
-                Submit
-              </Button>
-            </Link>
-          </div>
-        </Form>
-      </div>
+      ></div>
     )
   }
 
