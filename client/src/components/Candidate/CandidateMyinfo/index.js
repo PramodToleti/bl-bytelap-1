b3c26e6763676253c0262be4197a7c8f20714f0d
PramodToleti <pramodraina047@gmail.com>
2023-04-20 20:24:33 +0530

    Updated Candidate info with DB data

--- client/src/components/Candidate/CandidateMyinfo/index.js
+++ client/src/components/Candidate/CandidateMyinfo/index.js
@@ -2,15 +2,51 @@ import Stack from "react-bootstrap/Stack"
 import Button from "react-bootstrap/Button"
 import Form from "react-bootstrap/Form"
 
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
 import { Link } from "react-router-dom"
 import CandidateHome from "../CandidateHome"
 import ChooseFile from "../../../assets/ChooseFile"
+import Cookies from "js-cookie"
 
 function CandidateMyinfo() {
   const [validated, setValidated] = useState(false)
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    emailId: "",
+    mobileNumber: "",
+    city: "",
+  })
+
+  useEffect(() => {
+    async function fetchData() {
+      const token = Cookies.get("userToken")
+      const userId = localStorage.getItem("userId")
+
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ userId }),
+      }
+
+      const response = await fetch(
+        "http://localhost:5000/candidate/my-info",
+        options
+      )
+      const data = await response.json()
+      if (response.ok) {
+        setData(data)
+      } else {
+        console.log(data)
+      }
+    }
+    fetchData()
+  }, [])
 
   const handleSubmit = (event) => {
     const form = event.currentTarget
@@ -25,157 +61,161 @@ function CandidateMyinfo() {
   return (
     <>
       <CandidateHome />
-      <div
-        className="col-lg-4 col-md-4 search-course-right   mb-4 mt-4 p-2      border-secondary rounded container reveal  p-3 mb-5   rounded border border-secondary"
-        style={{ backgroundColor: "white" }}
-      >
-        <p className="text-center">My Info</p>
-
-        <Form
-          action=""
-          noValidate
-          validated={validated}
-          onSubmit={handleSubmit}
+      <div style={{ margin: "15px" }}>
+        <div
+          className="col-lg-4 col-md-4 search-course-right   mb-4 mt-4 p-2      border-secondary rounded container reveal  p-3 mb-5   rounded border border-secondary"
+          style={{ backgroundColor: "white" }}
         >
-          <Row className="mb-3">
-            <Form.Group as={Col} md="12" controlId="validationCustom01">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="First name"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter your first.
-              </Form.Control.Feedback>
-            </Form.Group>
+          <p className="text-center">My Info</p>
 
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label>Last Name</Form.Label>
-              <Form.Control
-                required
-                type="text"
-                placeholder="Last name"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter you last name.
-              </Form.Control.Feedback>
-            </Form.Group>
+          <Form
+            action=""
+            noValidate
+            validated={validated}
+            onSubmit={handleSubmit}
+          >
+            <Row className="mb-3">
+              <Form.Group as={Col} md="12" controlId="validationCustom01">
+                <Form.Label>First Name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="First name"
+                  defaultValue={data.firstName}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your first.
+                </Form.Control.Feedback>
+              </Form.Group>
 
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label>Email ID</Form.Label>
-              <Form.Control
-                required
-                type="Email"
-                placeholder="Email id"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter your email id.
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label>Mobile Number</Form.Label>
-              <Form.Control
-                required
-                type="number"
-                placeholder="Number"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter your number
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Form.Label>City</Form.Label>
-              <Form.Control
-                required
-                type="city"
-                placeholder="city"
-                defaultValue=""
-              />
-              <Form.Control.Feedback type="invalid">
-                Please enter your city
-              </Form.Control.Feedback>
-            </Form.Group>
-
-            <Form.Group
-              as={Col}
-              md="12"
-              className="mt-3"
-              controlId="validationCustom01"
-            >
-              <Stack
-                direction="horizontal"
-                gap={3}
-                style={{
-                  marginTop: "7px",
-                  display: "flex",
-                  alignItems: "flex-start",
-                }}
+              <Form.Group
+                as={Col}
+                md="12"
+                className="mt-3"
+                controlId="validationCustom01"
               >
-                <input className="d-none" type="file" />
-                <ChooseFile />
-                <div className="or-container">
-                  {" "}
-                  <p
-                    style={{
-                      marginTop: "8px",
-                    }}
-                  >
-                    OR
-                  </p>
-                </div>
-                <Link to="/create-resume">
-                  <button className=" btn btn-outline-secondary">Create</button>
-                </Link>
-              </Stack>
-              <Form.Control.Feedback type="invalid">
-                Please upload your resume.
-              </Form.Control.Feedback>
-            </Form.Group>
-            <small className="text-start text-muted mt-3">
-              PDF,Doc,Docx, | Max:2MB
-            </small>
-          </Row>
+                <Form.Label>Last Name</Form.Label>
+                <Form.Control
+                  required
+                  type="text"
+                  placeholder="Last name"
+                  defaultValue={data.lastName}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter you last name.
+                </Form.Control.Feedback>
+              </Form.Group>
 
-          <div className="d-grid gap-2 mt-5">
-            <Link to="/candidate">
-              <Button
-                type="submit"
-                variant="primary"
-                size="lg"
-                style={{ width: "100%" }}
+              <Form.Group
+                as={Col}
+                md="12"
+                className="mt-3"
+                controlId="validationCustom01"
               >
-                Save
-              </Button>
-            </Link>
-          </div>
-        </Form>
+                <Form.Label>Email ID</Form.Label>
+                <Form.Control
+                  required
+                  type="Email"
+                  placeholder="Email id"
+                  defaultValue={data.emailId}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your email id.
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group
+                as={Col}
+                md="12"
+                className="mt-3"
+                controlId="validationCustom01"
+              >
+                <Form.Label>Mobile Number</Form.Label>
+                <Form.Control
+                  required
+                  type="number"
+                  placeholder="Number"
+                  defaultValue={data.mobileNumber}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your number
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group
+                as={Col}
+                md="12"
+                className="mt-3"
+                controlId="validationCustom01"
+              >
+                <Form.Label>City</Form.Label>
+                <Form.Control
+                  required
+                  type="city"
+                  placeholder="city"
+                  defaultValue={data.city}
+                />
+                <Form.Control.Feedback type="invalid">
+                  Please enter your city
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group
+                as={Col}
+                md="12"
+                className="mt-3"
+                controlId="validationCustom01"
+              >
+                <Stack
+                  direction="horizontal"
+                  gap={3}
+                  style={{
+                    marginTop: "7px",
+                    display: "flex",
+                    alignItems: "flex-start",
+                  }}
+                >
+                  <input className="d-none" type="file" />
+                  <ChooseFile />
+                  <div className="or-container">
+                    {" "}
+                    <p
+                      style={{
+                        marginTop: "8px",
+                      }}
+                    >
+                      OR
+                    </p>
+                  </div>
+                  <Link to="/create-resume">
+                    <button className=" btn btn-outline-secondary">
+                      Create
+                    </button>
+                  </Link>
+                </Stack>
+                <Form.Control.Feedback type="invalid">
+                  Please upload your resume.
+                </Form.Control.Feedback>
+              </Form.Group>
+              <small className="text-start text-muted mt-3">
+                PDF,Doc,Docx, | Max:2MB
+              </small>
+            </Row>
+
+            <div className="d-grid gap-2 mt-5">
+              <Link to="/candidate">
+                <Button
+                  type="submit"
+                  variant="primary"
+                  size="lg"
+                  style={{ width: "100%" }}
+                >
+                  Save
+                </Button>
+              </Link>
+            </div>
+          </Form>
+        </div>
       </div>
     </>
   )
