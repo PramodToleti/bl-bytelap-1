323b688be0410e900fb8792d809e5a6d69a6e9dc
PramodToleti <pramodraina047@gmail.com>
2023-05-01 12:47:10 +0530

    Updated languages data in registration

--- client/src/assets/LanguageDropdown/index.js
+++ client/src/assets/LanguageDropdown/index.js
@@ -8,42 +8,131 @@ const skills = [
   { value: "english-intermediate", label: "English (Intermediate)" },
   { value: "english-fluent", label: "English (Fluent)" },
   { value: "english-native", label: "English (Native)" },
+
   { value: "telugu-beginner", label: "Telugu (Beginner)" },
   { value: "telugu-intermediate", label: "Telugu (Intermediate)" },
   { value: "telugu-fluent", label: "Telugu (Fluent)" },
   { value: "telugu-native", label: "Telugu (Native)" },
+
   { value: "hindi-beginner", label: "Hindi (Beginner)" },
   { value: "hindi-intermediate", label: "Hindi (Intermediate)" },
   { value: "hindi-fluent", label: "Hindi (Fluent)" },
   { value: "hindi-native", label: "Hindi (Native)" },
+
   { value: "tamil-beginner", label: "Tamil (Beginner)" },
   { value: "tamil-intermediate", label: "Tamil (Intermediate)" },
   { value: "tamil-fluent", label: "Tamil (Fluent)" },
   { value: "tamil-native", label: "Tamil (Native)" },
+
+  { value: "Marathi-beginner", label: "Marathi (Beginner)" },
+  { value: "Marathi-intermediate", label: "Marathi (Intermediate)" },
+  { value: "Marathi-fluent", label: "Marathi (Fluent)" },
+  { value: "Marathi-native", label: "Marathi (Native)" },
+
+  { value: "Bengali-beginner", label: "Bengali (Beginner)" },
+  { value: "Bengali-intermediate", label: "Bengali (Intermediate)" },
+  { value: "Bengali-fluent", label: "Bengali (Fluent)" },
+  { value: "Bengali-native", label: "Bengali (Native)" },
+
+  { value: "Urdu-beginner", label: "Urdu (Beginner)" },
+  { value: "Urdu-intermediate", label: "Urdu (Intermediate)" },
+  { value: "Urdu-fluent", label: "Urdu (Fluent)" },
+  { value: "Urdu-native", label: "Urdu (Native)" },
+
+  { value: "Gujarati-beginner", label: "Gujarati (Beginner)" },
+  { value: "Gujarati-intermediate", label: "Gujarati (Intermediate)" },
+  { value: "Gujarati-fluent", label: "Gujarati (Fluent)" },
+  { value: "Gujarati-native", label: "Gujarati (Native)" },
+
+  { value: "Kannada-beginner", label: "Kannada (Beginner)" },
+  { value: "Kannada-intermediate", label: "Kannada (Intermediate)" },
+  { value: "Kannada-fluent", label: "Kannada (Fluent)" },
+  { value: "Kannada-native", label: "Kannada (Native)" },
+
+  { value: "Oriya-beginner", label: "Oriya (Beginner)" },
+  { value: "Oriya-intermediate", label: "Oriya (Intermediate)" },
+  { value: "Oriya-fluent", label: "Oriya (Fluent)" },
+  { value: "Oriya-native", label: "Oriya (Native)" },
+
+  { value: "Punjabi-beginner", label: "Punjabi (Beginner)" },
+  { value: "Punjabi-intermediate", label: "Punjabi (Intermediate)" },
+  { value: "Punjabi-fluent", label: "Punjabi (Fluent)" },
+  { value: "Punjabi-native", label: "Punjabi (Native)" },
+
+  { value: "Malayalam-beginner", label: "Malayalam (Beginner)" },
+  { value: "Malayalam-intermediate", label: "Malayalam (Intermediate)" },
+  { value: "Malayalam-fluent", label: "Malayalam (Fluent)" },
+  { value: "Malayalam-native", label: "Malayalam (Native)" },
+
+  { value: "Assamese-beginner", label: "Assamese (Beginner)" },
+  { value: "Assamese-intermediate", label: "Assamese (Intermediate)" },
+  { value: "Assamese-fluent", label: "Assamese (Fluent)" },
+  { value: "Assamese-native", label: "Assamese (Native)" },
+
+  { value: "Kashmiri-beginner", label: "Kashmiri (Beginner)" },
+  { value: "Kashmiri-intermediate", label: "Kashmiri (Intermediate)" },
+  { value: "Kashmiri-fluent", label: "Kashmiri (Fluent)" },
+  { value: "Kashmiri-native", label: "Kashmiri (Native)" },
+
+  { value: "Sindhi-beginner", label: "Sindhi (Beginner)" },
+  { value: "Sindhi-intermediate", label: "Sindhi (Intermediate)" },
+  { value: "Sindhi-fluent", label: "Sindhi (Fluent)" },
+  { value: "Sindhi-native", label: "Sindhi (Native)" },
+
+  { value: "Sanskrit-beginner", label: "Sanskrit (Beginner)" },
+  { value: "Sanskrit-intermediate", label: "Sanskrit (Intermediate)" },
+  { value: "Sanskrit-fluent", label: "Sanskrit (Fluent)" },
+  { value: "Sanskrit-native", label: "Sanskrit (Native)" },
+
+  { value: "Konkani-beginner", label: "Konkani (Beginner)" },
+  { value: "Konkani-intermediate", label: "Konkani (Intermediate)" },
+  { value: "Konkani-fluent", label: "Konkani (Fluent)" },
+  { value: "Konkani-native", label: "Konkani (Native)" },
+
+  { value: "Manipuri-beginner", label: "Manipuri (Beginner)" },
+  { value: "Manipuri-intermediate", label: "Manipuri (Intermediate)" },
+  { value: "Manipuri-fluent", label: "Manipuri (Fluent)" },
+  { value: "Manipuri-native", label: "Manipuri (Native)" },
+
+  { value: "Nepali-beginner", label: "Nepali (Beginner)" },
+  { value: "Nepali-intermediate", label: "Nepali (Intermediate)" },
+  { value: "Nepali-fluent", label: "Nepali (Fluent)" },
+  { value: "Nepali-native", label: "Nepali (Native)" },
+
+  { value: "Bodo-beginner", label: "Bodo (Beginner)" },
+  { value: "Bodo-intermediate", label: "Bodo (Intermediate)" },
+  { value: "Bodo-fluent", label: "Bodo (Fluent)" },
+  { value: "Bodo-native", label: "Bodo (Native)" },
+
+  { value: "Santhali-beginner", label: "Santhali (Beginner)" },
+  { value: "Santhali-intermediate", label: "Santhali (Intermediate)" },
+  { value: "Santhali-fluent", label: "Santhali (Fluent)" },
+  { value: "Santhali-native", label: "Santhali (Native)" },
+
+  { value: "Maithili-beginner", label: "Maithili (Beginner)" },
+  { value: "Maithili-intermediate", label: "Maithili (Intermediate)" },
+  { value: "Maithili-fluent", label: "Maithili (Fluent)" },
+  { value: "Maithili-native", label: "Maithili (Native)" },
+
+  { value: "Dogri-beginner", label: "Dogri (Beginner)" },
+  { value: "Dogri-intermediate", label: "Dogri (Intermediate)" },
+  { value: "Dogri-fluent", label: "Dogri (Fluent)" },
+  { value: "Dogri-native", label: "Dogri (Native)" },
+
   { value: "russian-beginner", label: "Russian (Beginner)" },
   { value: "russian-intermediate", label: "Russian (Intermediate)" },
   { value: "russian-fluent", label: "Russian (Fluent)" },
   { value: "russian-native", label: "Russian (Native)" },
+
   { value: "turkish-beginner", label: "Turkish (Beginner)" },
   { value: "turkish-intermediate", label: "Turkish (Intermediate)" },
   { value: "turkish-fluent", label: "Turkish (Fluent)" },
   { value: "turkish-native", label: "Turkish (Native)" },
+
   { value: "spanish-beginner", label: "Spanish (Beginner)" },
   { value: "spanish-intermediate", label: "Spanish (Intermediate)" },
   { value: "spanish-fluent", label: "Spanish (Fluent)" },
   { value: "spanish-native", label: "Spanish (Native)" },
-  { value: "french-beginner", label: "French (Beginner)" },
-  { value: "french-intermediate", label: "French (Intermediate)" },
-  { value: "french-fluent", label: "French (Fluent)" },
-  { value: "french-native", label: "French (Native)" },
-  { value: "german-beginner", label: "German (Beginner)" },
-  { value: "german-intermediate", label: "German (Intermediate)" },
-  { value: "german-fluent", label: "German (Fluent)" },
-  { value: "german-native", label: "German (Native)" },
-  { value: "chinese-beginner", label: "Chinese (Beginner)" },
-  { value: "chinese-intermediate", label: "Chinese (Intermediate)" },
-  { value: "chinese-fluent", label: "Chinese (Fluent)" },
-  { value: "chinese-native", label: "Chinese (Native)" },
 ]
 
 const Option = (props) => {
@@ -139,7 +228,7 @@ class LanguageDropdown extends Component {
           <ReactSelect
             options={this.state.filteredSkills}
             isMulti
-            closeMenuOnSelect={false}
+            closeMenuOnSelect={true}
             hideSelectedOptions={false}
             components={{
               Option,
